4eaf34ec6ddce2697c915b28db67268d359df0c7
App Academy Student <student@appacademy.io>
2016-02-18 11:29:14 -0800

    Initial Project Setup

---
+++ frontend/pokedex.jsx
@@ -0,0 +1,8 @@
+var React = require('react'),
+    ReactDOM = require('react-dom');
+
+
+$(function () {
+document.addEventListener('DOMContentLoaded',
+ReactDOM.render(<p>Hello</p>, document.getElementById('root') ));
+});
